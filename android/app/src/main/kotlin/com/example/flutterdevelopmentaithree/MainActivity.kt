713f40a8452349e49d07b49082a14fd5e8c66fa8
Syed Syab <syabblogger@gmail.com>
2024-03-10 17:04:29 +0500

    project initiated

---
+++ android/app/src/main/kotlin/com/example/flutterdevelopmentaithree/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutterdevelopmentaithree
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/gettingstarted.dart
@@ -0,0 +1,14 @@
+// login_screen.dart
+import 'package:flutter/material.dart';
+
+class gettingstarted extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: // Your login UI here,
+    );
+  }
+}
